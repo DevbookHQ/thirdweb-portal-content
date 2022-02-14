a9e3bbe130cb11de1d36ff787c1f376fc947bad3
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-02-14 18:18:34 +0000

    Add docusaurus and Devbook componenents

---
+++ website/src/components/DevbookFileEditor.jsx
@@ -0,0 +1,59 @@
+import React, {
+  useEffect,
+  useCallback,
+} from 'react'
+
+import {
+  useDevbook,
+  DevbookStatus,
+  Env,
+} from '@devbookhq/sdk'
+import {
+  Editor as DevbookEditor,
+} from '@devbookhq/ui'
+
+export { Language } from '@devbookhq/ui'
+
+function DevbookFileEditor({
+  language,
+  filepath,
+  children: initialContent,
+}) {
+  const { fs, status } = useDevbook({ debug: true, env: Env.ThirdwebNode })
+
+  const updateContent = useCallback(async content => {
+    if (!fs) return
+    if (status !== DevbookStatus.Connected) return
+    if (!filepath) return
+
+    try {
+      await fs.write(filepath, content)
+    } catch (err) {
+      console.error(err)
+    }
+  }, [
+    fs,
+    status,
+    filepath,
+  ])
+
+  useEffect(function updateInitialContent() {
+    updateContent(initialContent)
+  }, [
+    initialContent,
+    updateContent,
+  ])
+
+  return (
+    <div className="dbk-editor-wrapper">
+      <DevbookEditor
+        filepath={filepath}
+        language={language}
+        initialContent={initialContent}
+        onContentChange={updateContent}
+      />
+    </div>
+  )
+}
+
+export default DevbookFileEditor
---
+++ website/src/components/DevbookRunnableCode.jsx
@@ -0,0 +1,93 @@
+import React, {
+  useState,
+  memo,
+  useCallback,
+  useEffect,
+} from 'react'
+
+import {
+  useDevbook,
+  DevbookStatus,
+  Env,
+} from '@devbookhq/sdk'
+import {
+  Editor,
+  Spinner,
+  Output,
+} from '@devbookhq/ui'
+
+export { Language } from '@devbookhq/ui'
+
+const libImport = `
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ethers } from "ethers";
+import { dotenv } from "dotenv";
+
+dotenv.config();
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.PRIVATE_KEY as string,
+    ethers.getDefaultProvider(process.env.PROVIDER_NETWORK as string),
+  )
+);
+
+`
+
+const MemoizedEditor = memo(Editor)
+
+function DevbookRunnableCode({
+  language,
+  children: initialCode,
+}) {
+  const { stdout, stderr, status, runCode, runCmd } = useDevbook({ debug: true, env: Env.ThirdwebNode })
+  const [code, setCode] = useState(initialCode)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if (stdout.length > 0 || stderr.length > 0) {
+      setIsLoading(false)
+    }
+  }, [stdout, stderr])
+
+  const run = useCallback(() => {
+    if (status !== DevbookStatus.Connected) return
+    setIsLoading(true)
+    runCode(libImport + code)
+    // runCmd(code)
+  }, [
+    code,
+    // runCmd,
+    runCode,
+    status,
+  ])
+
+  return (
+    <div className="dbk-editor-wrapper">
+      <div className="control-wrapper">
+        <button className="run-btn" onClick={run}>Run</button>
+        {isLoading &&
+          <div className="spin-wrapper">
+            <Spinner />
+          </div>
+        }
+      </div>
+      <MemoizedEditor
+        language={language}
+        initialContent={initialCode}
+        onContentChange={setCode}
+      />
+      {(stdout.length > 0 || stderr.length > 0) && (
+        <div style={{ paddingTop: '4px' }}>
+          <Output
+            stdout={stdout}
+            stderr={stderr}
+            height={250}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default DevbookRunnableCode
